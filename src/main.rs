5c4e6210984bd4222a4cbfe34ae78e649af988bc
Åke Forslund <ake.forslund@gmail.com>
2023-04-25 20:08:00 +0200

    Replace println with logs

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use std::{
     io::Error as IoError,
     net::SocketAddr,
     sync::{Arc, Mutex},
-    path::Path,
 };
+
+use log::{info,debug};
+use simple_logger;
 
 use futures_channel::mpsc::{unbounded, UnboundedSender};
 use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
@@ -24,7 +26,7 @@ async fn handle_connection(peer_map: PeerMap,
                            raw_stream: TcpStream,
                            addr: SocketAddr,
                            route: String) {
-    println!("Incoming TCP connection from: {}", addr);
+    info!("Incoming TCP connection from: {}", addr);
     let callback =  |req: &Request, res: Response|{
         if req.uri() != route.as_str() {
             let resp = Response::builder()
@@ -52,8 +54,8 @@ async fn handle_connection(peer_map: PeerMap,
     let (outgoing,  incoming) = ws_stream.split();
 
     let broadcast_incoming = incoming.try_for_each(|msg| {
-        //println!("Received a message from {}: {}",
-        //         addr, msg.to_text().unwrap());
+        debug!("Received a message from {}: {}",
+                 addr, msg.to_text().unwrap());
         if msg.is_text() || msg.is_binary() {
 
             let peers = peer_map.lock().unwrap();
@@ -74,26 +76,35 @@ async fn handle_connection(peer_map: PeerMap,
     pin_mut!(broadcast_incoming, receive_from_others);
     future::select(broadcast_incoming, receive_from_others).await;
 
-    println!("{} disconnected", &addr);
+    info!("{} disconnected", &addr);
     peer_map.lock().unwrap().remove(&addr);
 }
 
 
 #[tokio::main]
 async fn main () -> Result<(), IoError> {
-    let cfg = config::load(Path::new("./mycroft.conf"))?;
-    let host = cfg["websocket"]["host"].as_str().unwrap();
-    let port = cfg["websocket"]["port"].as_i64().unwrap();
-    let route = cfg["websocket"]["route"].as_str().unwrap().to_owned();
+    simple_logger::init_with_env().unwrap();
+
+    let cfg = config::ConfigStack::from_default().unwrap();
+    let val = cfg.get(&["websocket", "host"]).unwrap();
+    let host = val.as_str().unwrap();
+    let port = cfg.get(&["websocket", "port"]).unwrap().as_i64().unwrap();
+    let route = cfg.get(&["websocket", "route"]).unwrap()
+        .as_str().unwrap().to_owned();
     let bind_dest = format!("{}:{}", host, port);
-    println!("Starting at {}", bind_dest);
+    info!("Starting at {}", bind_dest);
+
+    let default = config::load_default().unwrap();
+    let default_ws = &default["websocket"];
+
+    info!("{}", default_ws["route"]);
 
     let state = PeerMap::new(Mutex::new(HashMap::new()));
 
     // Create the event loop and TCP listener we'll accept connections on.
     let try_socket = TcpListener::bind(&bind_dest).await;
     let listener = try_socket.expect("Failed to bind");
-    println!("Listening on: {}", bind_dest);
+    info!("Listening on: {}", bind_dest);
 
     // Let's spawn the handling of each connection in a separate task.
     while let Ok((stream, addr)) = listener.accept().await {
